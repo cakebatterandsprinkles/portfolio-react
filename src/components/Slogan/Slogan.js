9b279a33a986000d96d9f41d78dd76e4e9bf17fc
yagmurcetintas <yagmurcetin@gmail.com>
2019-09-05 19:58:57 -0400

    Added banner image

--- src/components/Slogan/Slogan.js
+++ src/components/Slogan/Slogan.js
@@ -4,7 +4,7 @@ import './Slogan.css';
 class Slogan extends React.Component {
     render(){
         return  <div className="flex items-center justify-center h-full">
-        <h1 className=" text-xl sm:text-2xl md:text-4xl xl:text-5xl text-white font-bold pb-2 font-mono">Ahoy! I be a Web DevelopArr!</h1>
+        <h1 className="slogan-text text-xl sm:text-2xl md:text-4xl xl:text-5xl text-white font-bold pb-2 font-mono">Ahoy! I be a Web DevelopArr!</h1>
         </div>
     }
 }

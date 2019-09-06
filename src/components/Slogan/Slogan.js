cb8a06dff0e3b1bf658c4def2d72e2221dc8e6a8
yagmurcetintas <yagmurcetin@gmail.com>
2019-09-06 11:57:10 -0400

    added slogan picture

--- src/components/Slogan/Slogan.js
+++ src/components/Slogan/Slogan.js
@@ -3,8 +3,8 @@ import './Slogan.css';
 
 class Slogan extends React.Component {
     render(){
-        return  <div className="flex items-center justify-center h-full">
-        <h1 className="slogan-text text-xl sm:text-2xl md:text-4xl xl:text-5xl text-white font-bold pb-2 font-mono">Ahoy! I be a Web DevelopArr!</h1>
+        return  <div className="flex items-center justify-center h-full slogan-text">
+        <h1 className="text-3xl md:text-4xl xl:text-5xl text-white pb-2">Ahoy! I be a Web DevelopArr!</h1>
         </div>
     }
 }

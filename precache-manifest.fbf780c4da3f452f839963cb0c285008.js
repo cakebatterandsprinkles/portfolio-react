531f1673fd1e9c80ccfc2ee57e06e099fe364aa2
yagmurcetintas <yagmurcetin@gmail.com>
2019-09-06 21:10:54 -0400

    Updates

---
+++ precache-manifest.fbf780c4da3f452f839963cb0c285008.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f2e43a5913068ede4105dbf1698b7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc05c171124cc56442f4",
+    "url": "/static/css/main.9c305879.chunk.css"
+  },
+  {
+    "revision": "171d752a27956ead0cef",
+    "url": "/static/js/2.3d98b647.chunk.js"
+  },
+  {
+    "revision": "cc05c171124cc56442f4",
+    "url": "/static/js/main.8fb58ea3.chunk.js"
+  },
+  {
+    "revision": "a5a15556c62e86917c5d",
+    "url": "/static/js/runtime~main.a1ed9fec.js"
+  },
+  {
+    "revision": "a01be10b757d9466384f04c55156f551",
+    "url": "/static/media/adventuretime-bg.a01be10b.png"
+  },
+  {
+    "revision": "2b9c32fcc597335885d834f167ecb73c",
+    "url": "/static/media/bamazon-01.2b9c32fc.png"
+  },
+  {
+    "revision": "fca8fc64c76c266f723134830b88e0d2",
+    "url": "/static/media/coding-blackbg.fca8fc64.png"
+  },
+  {
+    "revision": "beb5a7d2f3a9b17f14461096decf5489",
+    "url": "/static/media/crystal.beb5a7d2.png"
+  },
+  {
+    "revision": "f2bb4af3074726d82e9da72a35685d58",
+    "url": "/static/media/guessbird.f2bb4af3.jpg"
+  },
+  {
+    "revision": "6ffa44c98f9f52d1a94e08c8f3f48d26",
+    "url": "/static/media/hogwarts.6ffa44c9.png"
+  },
+  {
+    "revision": "d8d505c37a9cf623c5b6879b3fc0c06e",
+    "url": "/static/media/liribot.d8d505c3.jpg"
+  },
+  {
+    "revision": "bbb5f1c8d3604616e3a24598b5e32eb9",
+    "url": "/static/media/lumpyspace.bbb5f1c8.jpg"
+  },
+  {
+    "revision": "83055318912659b511d7b13f3c162abc",
+    "url": "/static/media/personal.83055318.jpg"
+  },
+  {
+    "revision": "3e82936f96157e9a3139d20343d33b0f",
+    "url": "/static/media/piratemamu2.3e82936f.jpg"
+  },
+  {
+    "revision": "b43b6cb85848bae9967ee58fe748feea",
+    "url": "/static/media/pupper.b43b6cb8.jpg"
+  },
+  {
+    "revision": "7a5bf85771c58ffa3f8187bde2d459de",
+    "url": "/static/media/train.7a5bf857.jpg"
+  }
+]);

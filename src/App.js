f997b821a557e274fdcfe1ca8a6012d30031084a
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-22 19:42:57 -0400

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./pages/MainPage";
+import {
+  createBrowserHistory
+} from "history";
+import ReactGA from "react-ga";
+
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({
+    page: location.pathname
+  });
+  ReactGA.pageview(location.pathname);
+});
 
 function App() {
-  return ( <MainPage />);
+  ReactGA.initialize("UA-155746861-1");
+  return ( < MainPage / > );
 }
 
 export default App;

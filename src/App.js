111c500429997509147f231de3c3c2d26671a170
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-22 19:50:25 -0400

    added a missing google analytics snippet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./pages/MainPage";
-import {
-  createBrowserHistory
-} from "history";
 import ReactGA from "react-ga";
 
-const history = createBrowserHistory();
-
-history.listen(location => {
-  ReactGA.set({
-    page: location.pathname
-  });
-  ReactGA.pageview(location.pathname);
-});
 
 function App() {
   ReactGA.initialize("UA-155746861-1");
+  ReactGA.pageview(window.location.pathname);
   return ( < MainPage / > );
 }
 
